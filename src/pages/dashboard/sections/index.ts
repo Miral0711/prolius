b8070633311310058380aba1b70ef50c8eb767e1
Miral Desai <desaimiral0711@gmail.com>
2026-04-17 12:08:25 +0530

    unused files removed

--- src/pages/dashboard/sections/index.ts
+++
@@ -1,6 +0,0 @@
-export { DashboardKpiSection } from './DashboardKpiSection';
-export { DashboardMapSection } from './DashboardMapSection';
-export { DashboardAnalyticsSection } from './DashboardAnalyticsSection';
-export { DashboardActivitySection } from './DashboardActivitySection';
-export { DashboardDriverSection } from './DashboardDriverSection';
-export { DashboardBottomSection } from './DashboardBottomSection';
